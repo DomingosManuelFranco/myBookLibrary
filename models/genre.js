e9a08b2a727345d07cdc68dd937b7ceacce21d9b
Your Name <manuelfranco070@gmail.com>
2021-05-24 10:53:12 +0100

    small changes in all files

--- models/genre.js
+++ models/genre.js
@@ -6,4 +6,4 @@ const GenreSchema = new Schema({
     name: {type: String, required: true, minLength: 3, maxLength: 100}
 });
 
-module.exports = mongoose.models('Genre', GenreSchema);
+module.exports = mongoose.model('Genre', GenreSchema);
