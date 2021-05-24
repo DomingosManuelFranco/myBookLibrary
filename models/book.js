d27fc494717d07986b6bdf06e3818fbccf4dd2ba
Your Name <manuelfranco070@gmail.com>
2021-05-24 10:08:24 +0100

    The Book Schema has created with success

---
+++ models/book.js
@@ -0,0 +1,19 @@
+const mongose = require('mongoose');
+
+const Schema = mongose.Schema;
+
+//Creating the Book Schema
+const BookSchema = new Schema({
+    title: {type: String, required: true},
+    author: {type: Schema.Types.ObjectId, ref: 'Author', required: true},
+    summary: {type: String, required: true},
+    isbn: {type: String, required: true},
+    genre: [{type: Schema.Types.ObjectId, ref: 'Genre'}]
+});
+
+//a virtual  for book's URL
+BookSchema.virtual('url').get( () => {
+    return 'catalog/book/' + this._id;
+});
+
+module.exports = mongose.model('Book', BookSchema);
